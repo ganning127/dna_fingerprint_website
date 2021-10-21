2428b07cd84b56db64403d852581a52c8cc1ed56
Ganning Xu <ganningxu@gmail.com>
2021-10-21 16:24:51 -0400

    homepage

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function init() {
+    // uncomment the below line for the typing animation
+    // new TypeIt("#welcome-text", {
+    //     speed: 50,
+    //     lifeLike: false
+    // }).go();
+}
+
+init();
