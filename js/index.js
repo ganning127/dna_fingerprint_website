0441fc0ec54c5d7a4f5316d0fe9133d5b474350a
Ganning Xu <ganningxu@gmail.com>
2021-10-21 21:46:35 -0400

    homepage and what is dna fingerprinting page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 function init() {
     // uncomment the below line for the typing animation
-    // new TypeIt("#welcome-text", {
-    //     speed: 50,
-    //     lifeLike: false
-    // }).go();
+    new TypeIt("#welcome-text", {
+        speed: 50,
+        // lifeLike: true,
+        waitUntilVisible: true,
+    }).go();
 }
 
 init();
