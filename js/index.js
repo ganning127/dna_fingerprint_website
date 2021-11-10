c66647b17e6b832ad4c5ec692e5ee63ce88ddc69
Ganning Xu <ganningxu@gmail.com>
2021-11-10 01:52:09 -0500

    not added last time

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 function init() {
     // uncomment the below line for the typing animation
     new TypeIt("#welcome-text", {
-        speed: 1,
+        speed: 50,
         // lifeLike: true,
         waitUntilVisible: true,
-        strings: 'Welcome <span class="bold text-light-green">detective</span>, it seems like you are here to learn more about the ethics behind <span class="bold text-light-green">DNA fingerprinting</span>...'
+        strings: 'Welcome <span class="bold text-light-green">detective</span>, it seems like you are here to learn more about the ethics behind <span class="bold text-light-green">DNA fingerprinting</span>...',
+        afterComplete: async () => {
+            // Will fire after the entire instance has completed typing.
+            // NOTE: If "loop" is enabled, this will never fire.
+
+            document.getElementById("navigation").classList.add("visible");
+        },
+
     }).go();
 }
 
